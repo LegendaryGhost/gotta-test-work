2abdff8bbca38872b65d0149f66bab2725517f58
Rihantiana Mbolatsiory <108069670+LegendaryGhost@users.noreply.github.com>
2024-04-23 16:41:02 +0300

    shorter deployement script

--- deploy.bat
+++ deploy.bat
@@ -7,7 +7,7 @@ set "temp=%work_dir%\temp"
 set "web=%work_dir%\web"
 set "web_xml=%work_dir%\web.xml"
 set "lib=%work_dir%\lib"
-set "web_apps=C:\xampp\tomcat\webapps"
+set "web_apps=C:\Program Files\Apache Software Foundation\Tomcat 10.1\webapps"
 set "war_name=gotta-taste"
 set "src=%work_dir%\src"
 
@@ -33,16 +33,10 @@ xcopy /s /i "%lib%\*.jar" "%temp%\WEB-INF\lib"
 :: Note: Assurez-vous que le chemin vers le compilateur Java (javac) est correctement configuré dans votre variable d'environnement PATH.
 :: Créer une liste de tous les fichiers .java dans le répertoire src et ses sous-répertoires
 dir /s /B "%src%\*.java" > sources.txt
-:: Créer une liste de tous les fichiers .jar dans le répertoire lib et ses sous-répertoires
-dir /s /B "%lib%\*.jar" > libs.txt
-:: Construire le classpath
-set "classpath="
-for /F "delims=" %%i in (libs.txt) do set "classpath=!classpath!%%i;"
 :: Exécuter la commande javac
-javac -d "%temp%\WEB-INF\classes" -cp "%classpath%" @sources.txt
+javac -d "%temp%\WEB-INF\classes" -cp "%lib%\*" @sources.txt
 :: Supprimer les fichiers sources.txt et libs.txt après la compilation
 del sources.txt
-del libs.txt
 
 :: Créer un fichier .war nommé [war_name].war à partir du dossier [temp] et son contenu dans le dossier [work_dir]
 cd "%temp%"
